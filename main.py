b2c6f32a09933bd07c0bb60b3ea8f4875915396b
jackpurrin <me@jackpurrin.me>
2025-04-23 21:59:44 -0700

    starts it

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world!");
+import time
+import base64
+
+shred1 = str(hash(time.time()))
+shred1bytes = shred1.encode("ascii")
+shred2 = base64.b64encode(shred1bytes)
+print(shred1)
+print(shred2)
+
